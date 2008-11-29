3672422b56000ea973ebef666f50ddc35cb6a697
Phill MV <phill.mv@gmail.com>
2008-11-29 18:26:04 -0500

    The universe's most simple gem gets TWICE AS COMPLICATED

--- lib/mediawiki.rb
+++ lib/mediawiki.rb
@@ -2,13 +2,20 @@ $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
 require 'rubygems'
-require 'nokogiri'
+#require 'nokogiri'
 require 'open-uri'
 
 module Mediawiki
-  VERSION = '0.0.2'
+  VERSION = '0.0.3'
   
   def self.search_for_html(wiki_host, term)
-    open("http://#{wiki_host}/wiki/Special:Search?search=#{term}")
+    open("http://#{wiki_host}/wiki/Special:Search?search=#{URI.encode(term)}").read
+  end
+
+  def self.last_modified(wiki_host, page)
+    # can't seem to find evidence that the query will ever return
+    # more than one page. Conveniently returns nil on pages that
+    # do not exist/throw errors.
+    YAML.load(open("http://#{wiki_host}/w/api.php?action=query&titles=#{URI.encode(page)}&format=yaml&prop=info").read)["query"]["pages"][0]["lastrevid"]
   end
 end
